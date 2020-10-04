cb82e946875684ab76849fe6a46ea8b7d05425dc
escc1122 <escc1122@gamil.com>
2020-10-04 17:49:43 +0800

    test2

--- aop/_30_Annotation/ClientStart.java
+++ aop/_30_Annotation/ClientStart.java
@@ -1,11 +1,4 @@
 package _30_Annotation;
-
-//import static_proxy_aop.HelloSpeaker;
-//import _3_Annotation.HelloSpeaker;
-//import static_proxy_aop.InterfaceHello;
-
-
-
 public class ClientStart {
     public static void main(String[] args){
         ReflectAnnotation reflectAnnotation = new ReflectAnnotation();
@@ -15,9 +8,5 @@ public class ClientStart {
         helloSpeaker.hello("Justin");
 
         helloSpeaker.hello2("Justin");
-
-//        InterfaceHello helloProxy =
-//                (InterfaceHello) reflectAnnotation.bind(new HelloSpeaker());
-//        helloProxy.hello("Justin");
     }
 }
